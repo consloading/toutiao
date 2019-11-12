204df1505bff5c7adc73ed5035fcf695a541e60c
consloading <wangyang9789@gmail.com>
2019-11-12 16:22:22 +0800

    导入commit完成

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)// 使用elementui
 Vue.config.productionTip = false
 
 new Vue({
